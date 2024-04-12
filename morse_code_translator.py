6c5797ba736cac7b293530b2876cbf167af5496b
UnrealDrip <97160407+UnrealDrip@users.noreply.github.com>
2024-04-12 15:45:05 +0200

    Update morse_code_translator.py

--- morse_code_translator.py
+++ morse_code_translator.py
@@ -1,3 +1,7 @@
+import winsound
+
+winsound.Beep(500,1000)
+
 input_string = input("Enter Morse code:")
 morse_string = ""
 
@@ -44,4 +48,12 @@ for letter in input_string.upper():
     morse_string += morse_dict[letter]
     morse_string += " "
    
-print(morse_string)
+for letter in morse_string:
+    if letter == '.':
+        winsound.Beep(800,500)
+    if letter == '-':
+        winsound.Beep(800,1000)
+    if letter == '/':
+        winsound.Beep(50,1000)
+    if letter == ' ':
+        winsound.Beep(50,300)
